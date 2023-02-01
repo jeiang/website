fdc76f6ad947221fc7b3aa682363f343417dcec9
Aidan Pinard <aidan@aidanpinard.co>
2023-02-01 14:11:27 -0400

    Memoized Github Stats & removed dependency on dark_light
    Signed-off-by: Aidan Pinard <aidan@aidanpinard.co>

--- src/components/github_stats.rs
+++ src/components/github_stats.rs
@@ -1,50 +1,47 @@
-use dark_light::{detect, Mode};
-
 use yew::prelude::*;
 use yew::Properties;
 
-const OVERVIEW_LIGHT_URL: &str =
-    "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/overview.svg#gh-light-mode-only";
-const OVERVIEW_DARK_URL: &str =
-    "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/overview.svg#gh-dark-mode-only";
-
-const LANGUAGES_LIGHT_URL: &str = 
-    "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/languages.svg#gh-light-mode-only";
-const LANGUAGES_DARK_URL: &str = 
-    "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/languages.svg#gh-dark-mode-only";
-
-#[derive(Properties, PartialEq, Eq, PartialOrd, Ord)]
-pub struct GHStatsProps {
-    #[prop_or(matches!(detect(), Mode::Light))]
-    pub is_dark_mode: bool
+fn image_html(light_url: &str, dark_url: &str, alt_text: &str) -> Html {
+    let light_url = light_url.to_owned();
+    let dark_url = dark_url.to_owned();
+    let alt_text = alt_text.to_owned();
+    
+    html! {
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jeiang">
+            <picture>
+                <source srcset={dark_url} media="(prefers-color-scheme: dark)"/>
+                <img src={light_url} alt={alt_text} />
+            </picture>
+        </a>
+    }
 }
 
 #[function_component(GHStatsOverview)]
-pub fn overview(props: &GHStatsProps) -> Html {
-    let url = if props.is_dark_mode {
-        OVERVIEW_DARK_URL
-    } else {
-        OVERVIEW_LIGHT_URL
-    };
-    html! {
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jeiang">
-            <img src={url} alt="github statistics" />
-        </a>
-    }
+pub fn overview() -> Html {
+    const LIGHT_URL: &str =
+        "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/overview.svg#gh-light-mode-only";
+    const DARK_URL: &str =
+        "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/overview.svg#gh-dark-mode-only";
+    const ALT_TEXT: &str = 
+        "Github Statistics";
+    let stats_image = use_memo(|_| {
+        image_html(LIGHT_URL, DARK_URL, ALT_TEXT)
+    }, ());
+    (*stats_image).clone()
 }
 
 #[function_component(GHStatsLanguages)]
-pub fn languages(props: &GHStatsProps) -> Html {
-    let url = if props.is_dark_mode {
-        LANGUAGES_DARK_URL
-    } else {
-        LANGUAGES_LIGHT_URL
-    };
-    html! {
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jeiang">
-            <img src={url} alt="github statistics" />
-        </a>
-    }
+pub fn languages() -> Html {
+    const LIGHT_URL: &str = 
+        "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/languages.svg#gh-light-mode-only";
+    const DARK_URL: &str = 
+        "https://raw.githubusercontent.com/jeiang/github-stats/master/generated/languages.svg#gh-dark-mode-only";
+    const ALT_TEXT: &str =
+        "Github Language Usage Statistics";
+    let stats_image = use_memo(|_| {
+        image_html(LIGHT_URL, DARK_URL, ALT_TEXT)
+    }, ());
+    (*stats_image).clone()
 }
 
 

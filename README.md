140f3ffb10a573a457f14172c3d41b5bdfb17ccf
Aidan Pinard <aidan@aidanpinard.co>
2026-04-02 00:10:08 -0400

    chore: astro website w/ tailwind

---
+++ README.md
@@ -0,0 +1,22 @@
+# Website
+
+This is my website, made using Astro and Tailwind. A Nix Flake is provided for developing and running the website.
+
+## Commands
+
+All commands are run from the root of the project, from a terminal:
+
+| Command               | Action                                           |
+| :-------------------- | :----------------------------------------------- |
+| `bun install`         | Installs dependencies                            |
+| `bun dev`             | Starts local dev server at `localhost:4321`      |
+| `bun build`           | Build your production site to `./dist/`          |
+| `bun preview`         | Preview your build locally, before deploying     |
+| `bun astro ...`       | Run CLI commands like `astro add`, `astro check` |
+| `bun astro -- --help` | Get help using the Astro CLI                     |
+
+## Served From
+
+- [jeiang.dev](https://jeiang.dev)
+- [pinard.co.tt](https://pinard.co.tt)
+- [aidanpinard.co](https://aidanpinard.co)
--- flake.nix
+++ flake.nix
@@ -5,17 +5,86 @@
   };
 
   outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import (inputs.nixpkgs) { inherit system; });
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import (inputs.nixpkgs) {inherit system;};
       in {
         devShell = pkgs.mkShell {
-          buildInputs= with pkgs; [
+          buildInputs = with pkgs; [
             bun
-            bashInteractive
-            nixd
-            pkgs.typescript-language-server
+            astro-language-server
+            typescript-language-server
           ];
+        };
+
+        packages = let
+          src = ./.;
+          packageJson = pkgs.lib.importJSON "${src}/package.json";
+          version = packageJson.version;
+          pname = packageJson.name;
+
+          node_modules = pkgs.stdenv.mkDerivation {
+            pname = "${pname}_node-modules";
+            inherit src version;
+
+            nativeBuildInputs = with pkgs; [bun];
+            buildInputs = with pkgs; [nodejs-slim_latest];
+
+            dontConfigure = true;
+            dontFixup = true; # patchShebangs produces illegal path references in FODs
+
+            buildPhase = ''
+              runHook preBuild
+              export HOME=$TMPDIR
+              bun install --no-progress --frozen-lockfile
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/node_modules
+              mv node_modules $out/
+              runHook postInstall
+            '';
+
+            outputHash = "sha256-Tt7kqID0Q6h1/KGOvZv5s1kdjr+XgZggA/A5qOiiM5U=";
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+          };
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            inherit pname version src;
+
+            nativeBuildInputs = with pkgs; [
+              node_modules
+              nodejs-slim_latest
+              bun
+            ];
+
+            configurePhase = ''
+              runHook preConfigure
+              cp -a ${node_modules}/node_modules ./node_modules
+              chmod -R u+rw node_modules
+              chmod -R u+x node_modules/.bin
+              patchShebangs node_modules
+              export HOME=$TMPDIR
+              export PATH="$PWD/node_modules/.bin:$PATH"
+              bun astro telemetry disable
+              runHook postConfigure
+            '';
+
+            buildPhase = ''
+              runHook preBuild
+              bun run build
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              cp -pr --reflink=auto dist $out/
+              runHook postInstall
+            '';
+          };
         };
       }
     );
